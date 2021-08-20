c3ac4224cba64ce435dee6debb89283ca8a371d3
electrosalaf <51977981+electrosalaf@users.noreply.github.com>
2021-08-20 14:29:32 +0100

    Implemented material design

--- app/src/main/java/com/example/hngstage2/MainActivity.kt
+++ app/src/main/java/com/example/hngstage2/MainActivity.kt
@@ -11,9 +11,9 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    val name = findViewById<EditText>(R.id.nameText)
-    val displayButton = findViewById<Button>(R.id.displayButton)
-    val displayMessage = findViewById<TextView>(R.id.displayText)
+    val name = findViewById<EditText>(R.id.edit_text)
+    val displayButton = findViewById<Button>(R.id.submit_button)
+    val displayMessage = findViewById<TextView>(R.id.text_preview)
 
     displayButton.setOnClickListener {
       displayMessage.setText("Welcome to the HNGi8 Internship ${name.text}")
